4d42b1e7d6866b3cedd10a1c5cdd573e24ba0cf0
theY4Kman <they4kman@gmail.com>
2011-09-14 02:13:35 -0400

    Added placeholder texts for chatinputs; chatinput logic; role instructions title now with names

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 require(["/js/jquery-1.6.2.min.js", "net"], jQueryInit);
 
+var game_started = false;
 var status = null;
 var condition = null;
 var role = null;
@@ -62,6 +63,106 @@ function setButtonPressed(buttons)
 
 function jQueryInit()
 {
+    (function($){
+      var ph = "PLACEHOLDER-INPUT";
+      var phl = "PLACEHOLDER-LABEL";
+      var boundEvents = false;
+      var default_options = {
+        labelClass: 'placeholder'
+      };
+      
+      //check for native support for placeholder attribute, if so stub methods and return
+      var input = document.createElement("input");
+      if ('placeholder' in input) {
+        $.fn.placeholder = $.fn.unplaceholder = function(){}; //empty function
+        delete input; //cleanup IE memory
+        return;
+      };
+      delete input;
+
+      $.fn.placeholder = function(options) {
+        bindEvents();
+
+        var opts = $.extend(default_options, options)
+
+        this.each(function(){
+          var rnd=Math.random().toString(32).replace(/\./,'')
+            ,input=$(this)
+            ,label=$('<label style="position:absolute;display:none;top:0;left:0;"></label>');
+
+          if (!input.attr('placeholder') || input.data(ph) === ph) return; //already watermarked
+
+          //make sure the input tag has an ID assigned, if not, assign one.
+          if (!input.attr('id')) input.attr('id') = 'input_' + rnd;
+
+          label	.attr('id',input.attr('id') + "_placeholder")
+              .data(ph, '#' + input.attr('id'))	//reference to the input tag
+              .attr('for',input.attr('id'))
+              .addClass(opts.labelClass)
+              .addClass(opts.labelClass + '-for-' + this.tagName.toLowerCase()) //ex: watermark-for-textarea
+              .addClass(phl)
+              .text(input.attr('placeholder'));
+
+          input
+            .data(phl, '#' + label.attr('id'))	//set a reference to the label
+            .data(ph,ph)		//set that the field is watermarked
+            .addClass(ph)		//add the watermark class
+            .after(label);		//add the label field to the page
+
+          //setup overlay
+          itemIn.call(this);
+          itemOut.call(this);
+        });
+      };
+
+      $.fn.unplaceholder = function(){
+        this.each(function(){
+          var	input=$(this),
+            label=$(input.data(phl));
+
+          if (input.data(ph) !== ph) return;
+            
+          label.remove();
+          input.removeData(ph).removeData(phl).removeClass(ph);
+        });
+      };
+
+
+      function bindEvents() {
+        if (boundEvents) return;
+
+        //prepare live bindings if not already done.
+        $('.' + ph)
+          .live('click',itemIn)
+          .live('focusin',itemIn)
+          .live('focusout',itemOut);
+        bound = true;
+
+        boundEvents = true;
+      };
+
+      function itemIn() {
+        var input = $(this)
+          ,label = $(input.data(phl));
+
+        label.css('display', 'none');
+      };
+
+      function itemOut() {
+        var that = this;
+
+        //use timeout to let other validators/formatters directly bound to blur/focusout work first
+        setTimeout(function(){
+          var input = $(that);
+          $(input.data(phl))
+            .css('top', input.position().top + 'px')
+            .css('left', input.position().left + 'px')
+            .css('display', !!input.val() ? 'none' : 'block');
+        }, 200);
+      };
+
+    }(jQuery));
+
     // Set the UI to the default blanked out state
     resetProfileNames();
     $('button').addClass('notyours');
@@ -94,12 +195,43 @@ function jQueryInit()
     });
 
     events.bind('server:gamestart', function (data) {
+        if (game_started || data.role == undefined)
+            return;
+        game_started = true;
+        
         role = data.role;
         condition = data.condition;
         
         setCurrentProfile(role);
         $('#content').removeClass('disabled').addClass('enabled');
         $('#tmpl_instructions').tmpl(data).appendTo('#instructions_role .body');
+        $('#instructions_role .title span').html(role.capitalize());
+        
+        var buy_info = 'Chat between ' + (role == 'buyer' ? 'you' : 'buyer') +
+            ' and ' + (role == 'insurer' ? 'you' : 'insurer') + '.';
+        var sell_info = 'Chat between ' + (role == 'seller' ? 'you' : 'seller') +
+            ' and ' + (role == 'insurer' ? 'you' : 'insurer') + '.';
+        
+        $('#buyer_chatinput').attr('placeholder', buy_info);
+        $('#seller_chatinput').attr('placeholder', sell_info);
+        
+        $('.chatinput[placeholder]').placeholder();
+        
+        switch (role)
+        {
+        case 'buyer':
+            $('#buyer_chatinput').removeAttr('disabled');
+            break;
+        
+        case 'seller':
+            $('#seller_chatinput').removeAttr('disabled');
+            break;
+        
+        case 'insurer':
+            $('.chatinput').removeAttr('disabled');
+            break;
+        }
+        
     });
     
     events.bind('send_money_buyer_seller', function () {
@@ -136,6 +268,15 @@ function jQueryInit()
     }
     $('#buyer_chatinput').bind('keydown', keydown('buyer'))
     $('#seller_chatinput').bind('keydown', keydown('seller'))
+    
+    $('.chatinput').focus(function() {
+        if (this.value === this.defaultValue)
+            this.value = '';
+    })
+    .blur(function() {
+        if (this.value === '')
+            this.value = this.defaultValue;
+    });
     
     // Bind to the approve accept button
     $('#approve').click(function() {
